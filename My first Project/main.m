8626a192151c87c51813967f00237b02f2515b82
Allen Niere <AllenNiere@allensmacbookpro.ent.lolcentral.com>
2014-10-06 16:21:49 -0500

    Initial Commit

---
+++ My first Project/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  My first Project
+//
+//  Created by Allen Niere on 10/6/14.
+//  Copyright (c) 2014 Allen Niere. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
